42ef59f3d137ac35b1d7916372561e8a753c268b
ivalencius <ilanvalencius@gmail.com>
2023-06-28 16:24:22 -0400

    converted set_timestep

---
+++ naive-working-build/SiStER_set_timestep.jl
@@ -0,0 +1,19 @@
+function SiStER_set_timestep(dx,dy,vx,vy,PARAMS)
+# [dt_m]=SiStER_set_timestep(dx,dy,vx,vy,PARAMS)
+# sets the advection time step
+# J.-A. Olive; November 2014
+
+dt_m=(PARAMS.fracCFL*0.5*
+    min(
+        minimum(dx),
+        minimum(dy)
+        )./
+    max(
+        maximum(abs.(vx)),
+        maximum(abs.(vy))
+    ));
+
+
+return dt_m
+
+end
