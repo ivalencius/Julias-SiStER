4c1044f6412470d7eeb19518a5ce3e8f45318dc4
ivalencius <ilanvalencius@gmail.com>
2023-06-28 16:19:58 -0400

    fixed errors

--- naive-working-build/SiStER_interp_normal_nodes_to_markers.jl
+++ naive-working-build/SiStER_interp_normal_nodes_to_markers.jl
@@ -9,33 +9,34 @@ function SiStER_interp_normal_nodes_to_markers(varN,xc,yc,xm,ym,icn,jcn)
 
 Nx=length(xc)+1;
 Ny=length(yc)+1;
-(m, ~) = size(varN);
+(m, _) = size(varN);
 
-ic = icn;
-jc = jcn;
+ic = copy(icn);
+jc = copy(jcn);
 
-jc[xm .> xc[jc]] = jc[xm .> xc[jc]]+1;
-jc[jc.<2] = 2;
-jc[jc.>(Nx-1)] = Nx-1;
+jc[xm' .> xc[jc]] = jc[xm' .> xc[jc]].+1;
+jc[jc.<2] .= 2;
+jc[jc.>(Nx-1)] .= Nx-1;
 
-ic[ym.>yc[ic]] = ic[ym.>yc[ic]]+1;
-ic[ic.<2] = 2;
-ic[ic.>(Ny-1)] = Ny-1;
+ic[ym'.>yc[ic]] = ic[ym'.>yc[ic]].+1;
+ic[ic.<2] .= 2;
+ic[ic.>(Ny-1)] .= Ny-1;
 
-xNnodes = [xc[jc-1] xc[jc] xc[jc] xc[jc-1]]';
-yNnodes = [yc[ic-1] yc[ic-1] yc[ic] yc[ic]]';
+xNnodes = [xc[jc.-1]' xc[jc]' xc[jc]' xc[jc.-1]']';
+yNnodes = [yc[ic.-1]' yc[ic.-1]' yc[ic]' yc[ic]']';
 
 # IND = sub2ind(size(varN), ic, jc);
 s2i = LinearIndices(size(varN)); # reverse is CartesianIndices
 IND = zeros(length(ic));
 for ind ∈ 1:length(ic)
-    IND[ind] = s2i[icn[ind], jcn[ind]]
+    IND[ind] = s2i[ic[ind], jc[ind]]
 end
-IND = Int.(INDEX);
-VARnodes = [varN[IND] varN[IND+m] varN[IND+1+m] varN[IND+1]]';
+IND = Int.(IND);
+VARnodes = [varN[IND] varN[IND.+m] varN[IND.+(1+m)] varN[IND.+1]]';
 
 varm = SiStER_interp_grid_to_marker_vector(xNnodes,yNnodes,VARnodes,xm,ym);
 
 
 return varm
+
 end
